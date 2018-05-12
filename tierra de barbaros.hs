17601c3989febf68ac795e573d638e54ba151ce6
Abril del Burgo <38875814+abrildelburgo@users.noreply.github.com>
2018-05-12 00:24:15 -0300

    arregle issues

--- tierra de barbaros.hs
+++ tierra de barbaros.hs
@@ -3,7 +3,7 @@ type Habilidad = String
 type Accion = Barbaro -> Barbaro
 type EventoDeAventura = Barbaro -> Bool
 
-data Barbaro = Barbaro { nombre :: String, fuerza :: Int, habilidades :: [String], objetos :: [String] }
+data Barbaro = Barbaro { nombre :: String, fuerza :: Int, habilidades :: [String], objetos :: [Accion] }
 
 espadas :: Int -> Accion
 espadas peso unBarbaro = unBarbaro { fuerza = fuerza unBarbaro + 2 * peso }
@@ -15,33 +15,33 @@ amuletosMisticos tipoDeAmuletoMistico unaHabilidad unBarbaro | tipoDeAmuletoMist
 anadirHabilidad :: Habilidad -> Accion
 anadirHabilidad unaHabilidad unBarbaro = unBarbaro { habilidades = unaHabilidad : (habilidades unBarbaro) }
 
-varitasDefectuosas :: Habilidad -> Accion
-varitasDefectuosas unaHabilidad = eliminarObjetos . anadirHabilidad "magia"
+varitasDefectuosas :: Accion
+varitasDefectuosas = eliminarObjetos . anadirHabilidad "magia"
 
 eliminarObjetos :: Accion
 eliminarObjetos unBarbaro = unBarbaro { objetos = [] }
 
 ardilla :: Accion
-ardilla unBarbaro = unBarbaro
+ardilla = id
 
 cuerda :: Accion -> Accion -> Accion
-cuerda objeto1 objeto2 = objeto2 . objeto1
+cuerda = (.)
 
-dave = ("Dave", 100, ["tejer", "escribir Poesia"], ["ardilla", "libroPedKing"])
-jeff = ("Jeff", 30, ["cocinar", "programar", "luchar"], ["espada", "cuchara"])
+dave = ("Dave", 100, ["tejer", "escribir Poesia"], [ardilla, libroPedKing])
+jeff = ("Jeff", 30, ["cocinar", "programar", "luchar"], [espadas, ardilla])
 
 
 --PUNTO 2
 megafono :: Accion
-megafono unBarbaro = unBarbaro { habilidades = map (\habilidadConcatenada -> toUpper habilidadConcatenada) (concat habilidades unBarbaro) } 
+megafono unBarbaro = unBarbaro { habilidades = (map toUpper.concat) habilidades unBarbaro } 
 
 megafonoBarbarico :: Accion -> Accion -> Accion
-megafonoBarbarico objeto1 objeto2 = megafono . ardilla . cuerda objeto1 objeto2 
+megafonoBarbarico = megafono . ardilla . cuerda
  
 
 --PUNTO 3
 invasionDeSuciosDuendes :: EventoDeAventura
-invasionDeSuciosDuendes unBarbaro = elem "Escribir Poesia Atroz" (habilidades unBarbaro)
+invasionDeSuciosDuendes = elem "Escribir Poesia Atroz". habilidades
 
 cremalleraDelTiempo :: EventoDeAventura
 cremalleraDelTiempo = noTienePulgares
@@ -55,25 +55,37 @@ ritualDeFechorias = saqueo unBarbaro || gritoDeGuera poderDeGrito unBarbaro || c
 saqueo:: EventoDeAventura
 saqueo unBarbaro = (tieneHabilidad robar unBarbaro) && (tieneFuerza 80 unBarbaro)
 
-tieneHabilidad unaHabilidad unBarbaro = ( length . filter (==unaHabilidad) ) (habilidades unBarbaro) > 0
+tieneHabilidad :: Habilidad -> Barbaro -> Bool
+tieneHabilidad unaHabilidad =  elem unaHabilidad . habilidades
 
+tieneFuerza :: Int -> Barbaro -> Bool
 tieneFuerza cantidadDeFuerza unBarbaro = fuerza unBarbaro > cantidadDeFuerza
 
 gritoDeGuera :: Int -> Accion
 gritoDeGuera poderDeGrito unBarbaro = ( gritoIgualALetrasDeHabilidades poderDeGrito (habilidades unBarbaro) ) && ( relacionGritoObjetos poderDeGrito (objetos unBarbaro) 4 )
 
+gritoIgualALetrasDeHabilidades :: Int -> [Habilidad] -> Bool
 gritoIgualALetrasDeHabilidades poderDeGrito listaDeHabilidades = poderDeGrito == (length.head.concat) listaDeHabilidades
 
-relacionFuerzaObjetos poderDeGrito listaDeObjetos numeroDeCondicion = poderDeGrito == ((*numeroDeCondicion).length) listaDeObjetos
+relacionGritoObjetos :: Int -> [Accion] -> Int -> Bool
+relacionGritoObjetos poderDeGrito listaDeObjetos numeroDeCondicion = poderDeGrito == ((*numeroDeCondicion).length) listaDeObjetos
 
 caligrafia :: EventoDeAventura
 caligrafia unBarbaro = habilidadesTienenTresVocales habilidades unBarbaro && habilidadesComienzanMayuscula habilidades unBarbaro
 
-habilidadesTienenTresVocales listaDeHabilidades = all tieneTresVocales listaDeHabilidades
+habilidadesTienenTresVocales :: [Habilidad] -> Bool
+habilidadesTienenTresVocales = all tieneTresVocales
 
-tieneTresVocales unaHabilidad = (length . filter vocales) unaHabilidad > 3
+tieneTresVocales :: Habilidad -> Bool
+tieneTresVocales unaHabilidad = (length . filter esVocal) unaHabilidad > 3
 
+vocales = ['a', 'e', 'i', 'o', 'u']
+
+esVocal :: Char -> Bool
+esVocal letra :: elem letra vocales
+
+habilidadesComienzanMayuscula :: [Habilidad] -> Bool
 habilidadesComienzanMayuscula listaDeHabilidades = map (toUpper.head) listaDeHabilidades == map head listaDeHabilidades
 
-sobrevivientes :: [Barbaro] -> EventoDeAventura -> [Barbaro]
-sobrevivientes listaDeBarbaros aventura = filter aventura listaDeBarbaros
+sobrevivientes :: EventoDeAventura -> [Barbaro] -> [Barbaro]
+sobrevivientes aventura = filter aventura
